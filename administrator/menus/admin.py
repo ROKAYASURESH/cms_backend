0fdddf327b48d3b424c5b3a2c483c22034d94b7e
ROKAYASURESH <sureshrokaya761@gmail.com>
2026-06-13 11:27:51 +0545

    deploy

--- administrator/menus/admin.py
+++ administrator/menus/admin.py
@@ -2,7 +2,6 @@
 from django.contrib import admin
 from .models import Menu
 
-
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('title', 'parent', 'position', 'publish')
